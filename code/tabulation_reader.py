fcea3ebcb0b655dfb7e1fd9a163a879bf5cd8b0e
Facundo - Central <facusoto1998@gmail.com>
2022-05-13 16:13:04 -0300

    Agregado lector de tabulaciones, util para unir las lineas de texto

---
+++ code/tabulation_reader.py
@@ -0,0 +1,24 @@
+import re
+import csv
+
+file1 = open(file="output---Indentatios_as_tabulation.txt", mode="r", encoding="utf-8")
+content = file1.readlines()
+counts = dict()
+line_count = 0
+
+tabulation = '\t'
+
+for line in content:
+    line_count += 1
+    if tabulation in line:
+        counter = line.count(tabulation)
+        counts[line_count] = counter
+    else:
+        counts[line_count] = 0
+
+print("finalizado")
+
+file_object = open('tabs_in_file.csv', mode='w', encoding='utf-8', newline="")
+writer = csv.writer(file_object, delimiter = ",")
+for key, value in counts.items():
+    writer.writerow([key, value])
